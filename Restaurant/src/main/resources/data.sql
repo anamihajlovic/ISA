c152844699bf31d22fd9bd48e82b767ea9a9509d
Ana <vjfdspo>
2016-12-20 11:55:34 +0100

    Kreirani podaci kojima ce se popuniti tabela korisnici

---
+++ Restaurant/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+-- USER
+
+insert into users(email, password, firstName, lastName, address) values ('jelenakalabic@gmail.com', 'volimAnu', 'Jelena', 'Kalabic', 'Ulica i broj 12, Cerevic')
+insert into users(email, password, firstName, lastName, address) values ('tamaramrksic@gmail.com', 'iJaVolimAnu', 'Tamara', 'Mrksic', 'Ulica i broj bb, Veternik')
+insert into users(email, password, firstName, lastName, address) values ('anamihajlovic@gmail.com', 'pass', 'Ana', 'Mihajlovic', 'Ulica i broj 40, Novi Sad')
+
+-- SYSTEM_MANAGER
