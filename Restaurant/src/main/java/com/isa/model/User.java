531539dadcdf906df3f9d40359eabe75145cd0c5
Ana <vjfdspo>
2016-12-20 10:07:40 +0100

    Kreirana apstraktna klasa za korisnike

---
+++ Restaurant/src/main/java/com/isa/model/User.java
@@ -0,0 +1,12 @@
+package com.isa.model;
+
+public abstract class User {
+
+	private String email;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String address;
+	
+	
+}
