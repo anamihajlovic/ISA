635564be83af10716bdf2d2f15e7f61b50f9a8be
Jelena K <khkj>
2017-02-08 22:03:25 +0100

    Data.sql

--- Restaurant/src/main/java/com/isa/model/Guest.java
+++ Restaurant/src/main/java/com/isa/model/Guest.java
@@ -1,13 +1,27 @@
 package com.isa.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="guests")
 public class Guest extends User {
-	
+	@Column
+	private String test;
 	
 
 	public Guest() {}
+
+
+	public String getTest() {
+		return test;
+	}
+
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+	
+	
 }
--- Restaurant/src/main/resources/data.sql
+++ Restaurant/src/main/resources/data.sql
@@ -4,10 +4,7 @@ insert into users(email, password, first_name, last_name, address) values ('tama
 insert into users(email, password, first_name, last_name, address) values ('anamihajlovic@gmail.com', 'pass', 'Ana', 'Mihajlovic', 'Ulica i broj 40, Novi Sad')
 
 --GUESTS
-insert into users(email, password, first_name, last_name, address) values ('peraperic@gmail.com', 'gost', 'Petar', 'Petrovic', 'Ulica i broj 12, Cerevic')
-insert into users(email, password, first_name, last_name, address) values ('mikamikic@gmail.com', 'gost', 'Mika', 'Mikic', 'Ulica i broj bb, Veternik')
-
-
+insert into guests(id, test) values (1, 'gdg')
 
 -- RESTAURANTS
 insert into restaurants(name, restaurant_type, rating, rating_counter) values ('La forzza', 'italijanski', 0.0, 0)
