87877f2c98f0d2f5c9f14e52267a869c843dce61
Jelena K <khkj>
2017-02-26 13:22:15 +0100

    u data.sql dodate rezervacije.

--- Restaurant/src/main/resources/data.sql
+++ Restaurant/src/main/resources/data.sql
@@ -284,3 +284,11 @@ insert into segment_tables(segment_id,table_id) values(2,10)
 
 insert into restaurant_segments(restaurant_id, segment_id) values (1,1)
 insert into restaurant_segments(restaurant_id, segment_id) values (1,2)
+
+
+
+--RESERVATIONS
+insert into reservations(res_id, res_name, guest_id, date, start_time, end_time) values (1, 'Lanterna', 1, '2017-02-26', '17:30', '19:45')
+insert into reservations(res_id, res_name, guest_id, date, start_time, end_time) values (1, 'Lanterna', 1, '2017-02-26', '18:30', '20:00')
+insert into reservations(res_id, res_name, guest_id, date, start_time, end_time) values (1, 'Lanterna', 1, '2017-02-27', '17:30', '19:45')
+insert into reservations(res_id, res_name, guest_id, date, start_time, end_time) values (1, 'Lanterna', 1, '2017-02-28', '17:30', '19:45')
